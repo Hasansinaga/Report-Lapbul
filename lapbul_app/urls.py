5c8e3494edf7bfd8529152cec64e9f26a1c5fea4
Hasan Sinaga <sinagahasan17@gmail.com>
2025-08-19 10:49:20 +0700

    first-commit

---
+++ lapbul_app/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+app_name = 'lapbul_app'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('lapbul/', views.lapbul, name='lapbul'),
+    path('lapbul/form_0000/', views.form_0000, name='form_0000'),
+    path('lapbul/form_0001/', views.form_0001, name='form_0001'),
+    path('lapbul/form_0002/', views.form_0002, name='form_0002'),
+    path('lapbul/form_0003/', views.form_0003, name='form_0003'),
+    path('lapbul/form_0004/', views.form_0004, name='form_0004'),
+    path('lapbul/form_0005/', views.form_0005, name='form_0005'),
+    path('lapbul/form_0006/', views.form_0006, name='form_0006'),
+    path('lapbul/form_0007/', views.form_0007, name='form_0007'),
+    path('lapbul/form_0008/', views.form_0008, name='form_0008'),
+    path('lapbul/form_0009/', views.form_0009, name='form_0009'),
+    path('lapbul/form_0010/', views.form_0010, name='form_0010'),
+    path('lapbul/form_0011/', views.form_0011, name='form_0011'),
+    path('lapbul/form_0012/', views.form_0012, name='form_0012'),
+]
---
+++ lapbul_app/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'lapbul_app/index.html', {'title': 'Laporan Ekternal'})
+
+def lapbul(request):
+    return render(request, 'lapbul_app/lapbul.html', {'title': 'Lapbul OJK'})
+
+def form_0000(request):
+    return render(request, 'lapbul_app/form_0000.html', {'title': 'Lapbul Ojk'})
+
+def form_0001(request):
+    return render(request, 'lapbul_app/form_0001.html', {'title': 'Lapbul Ojk'})
+
+def form_0002(request):
+    return render(request, 'lapbul_app/form_0002.html', {'title': 'Lapbul Ojk'})
+
+def form_0003(request):
+    return render(request, 'lapbul_app/form_0003.html', {'title': 'Lapbul Ojk'})
+
+def form_0004(request):
+    return render(request, 'lapbul_app/form_0004.html', {'title': 'Lapbul Ojk'})
+
+def form_0005(request):
+    return render(request, 'lapbul_app/form_0005.html', {'title': 'Lapbul Ojk'})
+
+def form_0006(request):
+    return render(request, 'lapbul_app/form_0006.html', {'title': 'Lapbul Ojk'})
+
+def form_0007(request):
+    return render(request, 'lapbul_app/form_0007.html', {'title': 'Lapbul Ojk'})
+
+def form_0008(request):
+    return render(request, 'lapbul_app/form_0008.html', {'title': 'Lapbul Ojk'})
+
+def form_0009(request):
+    return render(request, 'lapbul_app/form_0009.html', {'title': 'Lapbul Ojk'})
+
+def form_0010(request):
+    return render(request, 'lapbul_app/form_0010.html', {'title': 'Lapbul Ojk'})
+
+def form_0011(request):
+    return render(request, 'lapbul_app/form_0011.html', {'title': 'Lapbul Ojk'})
+
+def form_0012(request):
+    return render(request, 'lapbul_app/form_0012.html', {'title': 'Lapbul Ojk'})
