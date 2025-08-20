8de653b6a6b639514c24de809ba2a1ea9bea8125
william-napitupulu <williamnapitupulu96@gmail.com>
2025-08-20 16:11:09 +0700

    Push_01_William
    feat(0013-0400)

--- lapbul_app/urls.py
+++ lapbul_app/urls.py
@@ -19,4 +19,22 @@ urlpatterns = [
     path('lapbul/form_0010/', views.form_0010, name='form_0010'),
     path('lapbul/form_0011/', views.form_0011, name='form_0011'),
     path('lapbul/form_0012/', views.form_0012, name='form_0012'),
+
+    # Start of Changes made by William 
+
+    path('lapbul/form_0013/', views.form_0013, name='form_0013'),
+    path('lapbul/form_0015/', views.form_0015, name='form_0015'),
+    path('lapbul/form_0016/', views.form_0016, name='form_0016'),
+    path('lapbul/form_0017/', views.form_0017, name='form_0017'),
+    path('lapbul/form_0018/', views.form_0018, name='form_0018'),
+    path('lapbul/form_0019/', views.form_0019, name='form_0019'),
+    path('lapbul/form_0020/', views.form_0020, name='form_0020'),
+    path('lapbul/form_0021/', views.form_0021, name='form_0021'),
+    path('lapbul/form_0100/', views.form_0100, name='form_0100'),
+    path('lapbul/form_0101/', views.form_0101, name='form_0101'),
+    path('lapbul/form_0200/', views.form_0200, name='form_0200'),
+    path('lapbul/form_0300/', views.form_0300, name='form_0300'),
+    path('lapbul/form_0400/', views.form_0400, name='form_0400'),
+
+    # End of changes made by William
 ]
--- lapbul_app/views.py
+++ lapbul_app/views.py
@@ -45,3 +45,46 @@ def form_0011(request):
 
 def form_0012(request):
     return render(request, 'lapbul_app/form_0012.html', {'title': 'Lapbul Ojk'})
+
+# Start of Changes made by William 
+
+def form_0013(request):
+    return render(request, 'lapbul_app/form_0013.html', {'title': 'Lapbul Ojk'})
+
+def form_0015(request):
+    return render(request, 'lapbul_app/form_0015.html', {'title': 'Lapbul Ojk'})
+
+def form_0016(request):
+    return render(request, 'lapbul_app/form_0016.html', {'title': 'Lapbul Ojk'})
+
+def form_0017(request):
+    return render(request, 'lapbul_app/form_0017.html', {'title': 'Lapbul Ojk'})    
+
+def form_0018(request):
+    return render(request, 'lapbul_app/form_0018.html', {'title': 'Lapbul Ojk'})
+
+def form_0019(request):
+    return render(request, 'lapbul_app/form_0019.html', {'title': 'Lapbul Ojk'})
+
+def form_0020(request):
+    return render(request, 'lapbul_app/form_0020.html', {'title': 'Lapbul Ojk'})
+
+def form_0021(request):
+    return render(request, 'lapbul_app/form_0021.html', {'title': 'Lapbul Ojk'})
+
+def form_0100(request):
+    return render(request, 'lapbul_app/form_0100.html', {'title': 'Lapbul Ojk'})
+
+def form_0101(request):
+    return render(request, 'lapbul_app/form_0101.html', {'title': 'Lapbul Ojk'})
+
+def form_0200(request):
+    return render(request, 'lapbul_app/form_0200.html', {'title': 'Lapbul Ojk'})
+
+def form_0300(request):
+    return render(request, 'lapbul_app/form_0300.html', {'title': 'Lapbul Ojk'})
+
+def form_0400(request):
+    return render(request, 'lapbul_app/form_0400.html', {'title': 'Lapbul Ojk'})
+
+# End of changes made by William
